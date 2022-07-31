cc8ccb05cd27c2d5cedfd361bdf1a3578bfa930f
KuligKamil <kamil.marek.kulig@gmail.com>
2022-07-31 21:10:02 +0200

    chore: get all data about links from code&fun

---
+++ main.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const path = '../kuligkamil.github.io/_posts/';
+const file = '2022-08-04-code-fun-old.md';
+const readline = require('readline');
+
+function isDate(line) {
+    let date = new Date(line.trim())
+    // console.log(typeof date)
+    return date.toString() !== 'Invalid Date';
+}
+
+function isEmpty(line) {
+    return line.trim() !== ''
+}
+
+function createFile(name, links) {
+    console.log(name, links.length)
+    // fs.writeFile('', 'Learn Node FS module', function (err) {
+    //     if (err) throw err;
+    //     console.log('File is created successfully.');
+    // });
+}
+
+function isLink(line) {
+    return line.trim().includes('http')
+}
+
+async function processLineByLine() {
+    try {
+        console.log('Code&Fun')
+        const fileStream = fs.createReadStream(`${path}${file}`);
+        const lines = readline.createInterface({
+            input: fileStream,
+            crlfDelay: Infinity
+            // Note: we use the crlfDelay option to recognize all instances of CR LF
+            // ('\r\n') in input.txt as a single line break.
+        });
+        this.linesNumber = 0
+        this.filesNumber = 0
+        this.links = []
+        this.date = ''
+        for await (const line of lines) {
+            if (isEmpty(line)) {
+                if (isDate(line) && !isLink(line)) {
+                    this.filesNumber++
+                    if (this.links.length > 0) {
+                        createFile(this.date, this.links)
+                        this.links = []
+                    }
+                    this.date = line.trim()
+                } else {
+                    this.links.push(line)
+                }
+                this.linesNumber++
+            }
+        }
+        createFile(this.date, this.links)
+        console.log(this.linesNumber)
+        console.log(this.filesNumber)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+processLineByLine()
+
+// TODO: monthly links about code&fun
+// TODO: to share
+// new Date(' http://siadamgadampelenserwis.pl:8501/ ')
+// Sat Jan 01 8501 00:00:00 GMT+0100 (Central European Standard Time)
+// new Date(' https://openai.com/dall-e-2/')
+// Thu Feb 01 2001 00:00:00 GMT+0100 (Central European Standard Time)
+// TODO: js vs python
+// append vs push
+// a++ vs a += 1
+// function vs def
