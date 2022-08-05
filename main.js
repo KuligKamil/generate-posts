5aa1ce2a2daf196e9302a5edba5559d487a56eaa
KuligKamil <kamil.marek.kulig@gmail.com>
2022-08-06 01:54:30 +0200

    dirty: get title from head title

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
-const path = '../kuligkamil.github.io/_posts/';
-const file = '2022-08-04-code-fun-old.md';
-const readline = require('readline');
-const puppeteer = require('puppeteer');
+const path = '../kuligkamil.github.io/_posts/'
+const file = '2022-08-04-code-fun-old.md'
+const readline = require('readline')
+const puppeteer = require('puppeteer')
 
 function isDate(line) {
     let date = new Date(line.trim())
@@ -15,7 +15,11 @@ function isEmpty(line) {
 }
 
 function createFile(name, links) {
-    console.log(name, links.length)
+    // console.log(name, links.length)
+    console.log(`# ${name} \n`)
+    for (let url in links) {
+        console.log(`* ${url} \n ${links[url]} \n\n`)
+    }
     // fs.writeFile('', 'Learn Node FS module', function (err) {
     //     if (err) throw err;
     //     console.log('File is created successfully.');
@@ -50,77 +54,46 @@ async function processLineByLine() {
     try {
         const browser = await puppeteer.launch({headless: false})
         let page
-        console.log('Code&Fun')
+        let linesNumber = 0
+        let filesNumber = 0
+        let links = {}
+        let date = ''
         const fileStream = fs.createReadStream(`${path}${file}`)
         const lines = readline.createInterface({
             input: fileStream,
             crlfDelay: Infinity
             // Note: we use the crlfDelay option to recognize all instances of CR LF
             // ('\r\n') in input.txt as a single line break.
-        });
-        let linesNumber = 0
-        let filesNumber = 0
-        let links = []
-        let date = ''
+        })
         for await (const line of lines) {
             if (!isEmpty(line)) {
+
                 if (isDate(line) && !isLink(line) && isNewMonth(date, line)) {
                     filesNumber++
-                    if (links.length > 0) {
+                    if (Object.keys(links).length > 0) {
                         createFile(date, links)
                         links = []
                     }
                     date = getDate(line)
+                } else if (isDate(line) && !isNewMonth(date, line)) {
+                    continue
                 } else {
+                    let title = ''
                     if (isLink(line)) {
                         page = await browser.newPage();
                         await page.goto(line, {waitUntil: 'networkidle2'})
-                        // const desc = getDescriptions()
-                        // console.log(desc)
-                        let list = await page.evaluate(() =>
-                            // {
-                            //     let descriptionProperties = ['og:description', 'twitter:description', 'description']
-                            //     for (let descriptionProperty in descriptionProperties) {
-                            //         descriptionProperty = document.querySelectorAll(`meta[property=\"${descriptionProperty}\"]`)
-                            //
-                            //         if (descriptionProperty !== null) {
-                            //             return descriptionProperty.content
-                            //         }
-                            //     }
-                            // })
-                            //
-                            Array.from(document.querySelectorAll('meta'),
-                                (e) => {
-                                    if (e.getAttributeNames().includes('property')) {
-                                        if (e.getAttribute('property').includes('desc')) {
-                                            return e.content
-                                        }
-                                    }
-                                    if (e.getAttributeNames().includes('name')) {
-                                        if (e.getAttribute('name').includes('desc')) {
-                                            return e.content
-                                        }
-                                    }
-                                }))
-                        //
-                        // // const descriptionTag = await page.$('meta[name="description"]')
-                        // // let a = 7
-                        // // const description = await descriptionTag?.getAttribute('content')
-                        list = list.filter(a => a !== null)
-                        console.log(list[0])
-                        desc = list.sort(function (a, b) {
-                            return b.length - a.length
-                        })[0];
-                        console.log(desc)
+                        title = await page.title()
+                        // console.log('title', title)
                     }
-                    links.push(line)
+                    links[line] = title
+                    // links.push({[line]: title})
                 }
                 linesNumber++
             }
         }
         createFile(date, links)
-        console.log(linesNumber)
-        console.log(filesNumber)
+        // console.log(linesNumber)
+        // console.log(filesNumber)
     } catch (e) {
         console.log(e)
     }
@@ -140,6 +113,10 @@ processLineByLine().then()
 // * Take GitHub to the command line
 // GitHub CLI brings GitHub to your terminal. Free and open source
 // https://cli.github.com/
+// * twine sugarcube
+// https://deno.land/
+// https://fresh.deno.dev/
+// * [line] <- brackets, using variable as a key
 // TODO: js vs python
 // append vs push
 // a++ vs a += 1
